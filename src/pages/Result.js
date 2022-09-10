caaede49ce3f22b6bd4bf0da04f92550dee7bdd7
shine729 <66360301+shine729@users.noreply.github.com>
2022-09-10 09:19:53 +0900

    add mobile style

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Button from "react-bootstrap/Button";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { ResultData } from "../assets/data/resultData";
-import { ButtonGroup } from 'react-bootstrap';
+// import { ButtonGroup } from 'react-bootstrap';
 import KakaoShareButton from '../component/KakaoShareButton';
 
   const Result = () => {
@@ -26,21 +26,22 @@ import KakaoShareButton from '../component/KakaoShareButton';
       {/* <Title>결과 보기</Title> */}
       <Name>당신은 {resultData.name}</Name>
       <LogoImage>
-        <img src={resultData.image} alt="결과이미지" width={450} height={450}/>
+        <img src={resultData.image} alt="결과이미지" width={350} height={350}/>
       </LogoImage>
       <Desc>{resultData.desc}입니다</Desc>
       <Fea>{resultData.fea}</Fea>
-      <ButtonGroup>
+      <Char>{resultData.char}</Char>
+      <Sick>{resultData.sick}</Sick>
+      <Buttongroup>
         <Button onClick={() => navigate("/")} className="btn-danger"
                 style={{
                   width: 170,
                   fontFamily: "SimKyungha",
-                  fontSize: 25,
+                  fontSize: 23,
                   marginTop: 20,
                 }}>테스트 다시하기</Button>
                 <KakaoShareButton data={resultData}/>
-      </ButtonGroup>
-              
+      </Buttongroup>
     </Contents>
   </Wrapper>
   )
@@ -50,7 +51,6 @@ export default Result;
 
 const Wrapper = styled.div`
   background-color:#9E5FE3;
-  height:120vh;
   width:100%;
 `
 
@@ -59,18 +59,20 @@ const Contents = styled.div`
   justify-content:center;
   align-items:center;
   flex-direction:column;
-  height:100vh;
 `
 
 const Header = styled.div`
   font-family: "SimKyungha";
   font-size:45px;
-  height:10vh;
   display:flex;
   justify-content:center;
   align-items:center;
   background:#8A20FA;
   color:#fff;
+  @media screen and (max-width: 992px) {
+    height:60px;
+    font-size:40px;
+}
 `
 
 // const Title = styled.div`
@@ -80,19 +82,27 @@ const Header = styled.div`
 // `
 
 const LogoImage = styled.div`
-margin-top:10px;
+margin-top:0;
 `
 
 const Desc = styled.div`
 font-family: "SimKyungha";
 font-size:25px;
 color:#fff;
+@media screen and (max-width: 992px) {
+  font-size:22px;
+  width:90%;
+}
 `
 const Name = styled.div`
 font-family: "SimKyungha";
 font-size:28px;
-margin-top:60px;
+margin-top:30px;
 color:#fff;
+@media screen and (max-width: 992px) {
+  margin-top:10px;
+  font-size:25px;
+}
 `
 
 const Fea = styled.div`
@@ -102,8 +112,47 @@ margin-top:15px;
 color:#fff;
 width:50%;
 white-space:break-spaces;
+@media screen and (max-width: 992px) {
+  width:90%;
+  font-size:20px;
+}
 `
+
+const Char = styled.div`
+font-family: "SimKyungha";
+font-size:22px;
+margin-top:15px;
+color:#fff;
+width:50%;
+white-space:pre-wrap;
+@media screen and (max-width: 992px) {
+  width:90%;
+  font-size:20px;
+}
+`
+
+const Sick = styled.div`
+font-family: "SimKyungha";
+font-size:22px;
+margin-top:15px;
+color:#fff;
+width:50%;
+white-space:pre-wrap;
+@media screen and (max-width: 992px) {
+  width:90%;
+  font-size:20px;
+}
+`
+
 const Buttongroup = styled.div`
   display:flex;
   flex-directon:row;
+  margin-bottom: 30px;
+  @media screen and (max-width: 992px) {
+    margin-bottom: 130px;
+    width:90%;
+  }
 `
+
+
+
