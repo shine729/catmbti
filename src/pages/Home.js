61627479a14e1871685aa6aae48cac9c01091c68
shine729 <66360301+shine729@users.noreply.github.com>
2022-09-11 06:36:12 +0900

    add image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import styled  from 'styled-components';//css사용시 필요함
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';//페이지이동
 import Fairy from '../assets/images/fairy.png';
+import Fairy2 from '../assets/images/fairy2.png';
 
 const Home = () =>{
   const Navigate = useNavigate();
@@ -11,20 +12,22 @@ const Home = () =>{
   }
   return (
     <Wrapper>
-      <Header>요정 판별기</Header>
+      <Header><img alt="요정사진" src={Fairy2} width={50} height={50}/>요정 판별기<img alt="요정사진" src={Fairy2} width={50} height={50}/></Header>
       <Contents>
-        <Title>나에게 성향에 맞는 요정은?</Title>
+        <Title>요정으로 태어난다면 당신은 어떤요정일까?</Title>
         <LogoImage>
           <img alt="요정사진" src={Fairy} width={350} height={350}/>
         </LogoImage>
         <Desc>MBTI를 기반으로하는 요정테스트</Desc>
-        <Desc2>요정으로 태어나면 당신은 어떤요정일까?</Desc2>
+        <Desc2>내 성향에 맞는 요정은?</Desc2>
         <Button onClick={handleClickButton} className="btn-danger"
             style={{
               fontFamily: "SimKyungha",
               fontSize: 23,
               marginTop: 20,
               marginBottom: 20,
+              backgroundColor:'#8A20FA',
+              borderColor:'#8A20FA'
             }}>테스트 시작하기</Button>
       </Contents>
     </Wrapper>
