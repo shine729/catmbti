25f4a5ce7fa790e63e10ad445de9ada6d426470f
shine729 <66360301+shine729@users.noreply.github.com>
2022-09-11 06:37:35 +0900

    change css style

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -46,12 +46,12 @@ const Question = () => {
 
   return (
     <Wrapper>
-      <ProgressBar striped variant="danger" now={questionNo / QuestionData.length * 100} />
+      <ProgressBar striped variant="info" now={questionNo / QuestionData.length * 100} />
       <Titlewrap>
         <Title>{QuestionData[questionNo].title}</Title>
         <ButtonGrounp>
-          <Button onClick={() => handleClickButton(1 , QuestionData[questionNo].type)} style={{width:'40%', minHeight:'210px', fontSize:'23px'}}>{QuestionData[questionNo].answera}</Button>
-          <Button onClick={() => handleClickButton(0 , QuestionData[questionNo].type)}  style={{width:'40%', minHeight:'210px', fontSize:'23px',marginLeft:'20px'}}>{QuestionData[questionNo].answerb}</Button>
+          <Button  onClick={() => handleClickButton(1 , QuestionData[questionNo].type)} style={{width:'100%', minHeight:'135px', fontSize:'23px', marginBottom:'20px', backgroundColor:'#8A20FA',borderColor:'#8A20FA'}}>{QuestionData[questionNo].answera}</Button>
+          <Button  onClick={() => handleClickButton(0 , QuestionData[questionNo].type)}  style={{width:'100%', minHeight:'135px', fontSize:'23px',backgroundColor:'#8A20FA',borderColor:'#8A20FA'}}>{QuestionData[questionNo].answerb}</Button>
         </ButtonGrounp>
       </Titlewrap>
     </Wrapper>
@@ -77,20 +77,27 @@ const Titlewrap = styled.div`
 const Title = styled.div`
   font-family: "SimKyungha";
   text-align:center;
-  font-size:40px;
+  font-size:36px;
   color:#fff;
-  width:80%;
+  width:50%;
   margin:0 auto;
   margin-bottom:10px;
+  white-space:break-spaces;
   @media screen and (max-width: 992px) {
-    font-size:28px;
+    font-size:27px;
+    width:90%;
   }
 `
 
 const ButtonGrounp = styled.div`
   font-family: "SimKyungha";
   display:flex;
-  flex-direction:row;
+  flex-direction:column;
   align-items:center;
   justify-content:center;
+  width:45%;
+  margin:0 auto;
+  @media screen and (max-width: 992px) {
+    width:90%;
+  }
 `
